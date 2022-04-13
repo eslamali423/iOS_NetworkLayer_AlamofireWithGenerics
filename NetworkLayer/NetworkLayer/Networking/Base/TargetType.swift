a1a0f96695951b3af88d23bcd54e6642fe82d6e9
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-04-13 22:16:11 +0200

    Create TargetType File and Customize HTTPMethod, TargetType, Task

---
+++ NetworkLayer/NetworkLayer/Networking/Base/TargetType.swift
@@ -0,0 +1,34 @@
+//
+//  TargetType.swift
+//  NetworkLayer
+//
+//  Created by Eslam Ali  on 13/04/2022.
+//
+
+import Foundation
+import Alamofire
+
+enum HTTPMethod : String {
+    
+    case get  = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    
+}
+enum Task   {
+    case requestPlain
+    
+    case requestParameter(parameters : [String : Any], encoding : ParameterEncoding)
+    
+}
+
+protocol TargetType  {
+   
+    var baseUrl : String {get}
+    var path : String {get}
+    var method  : HTTPMethod {get}
+    var task : Task {get}
+    var headers : [String:String]? {get}
+
+}
